a0cc8636c2a862d47d19ae56ff22cc68263fe679
vanderms <vanderms.84@gmail.com>
2022-03-09 17:05:14 -0300

    add title to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import type { NextPage } from 'next'
-
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
-    <main>Is it working?</main>
-  )
-}
+    <>
+      <Head>
+        <title>Frontend Mentor | Pod request access landing page</title>
+      </Head>
+      <main>Is it working?</main>
+    </>
+  );
+};
 
-export default Home
+export default Home;
