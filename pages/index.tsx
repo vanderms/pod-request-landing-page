a18842b3ad19db62a46b319d2c68eeb5ff3880af
vanderms <vanderms.84@gmail.com>
2022-03-10 09:59:55 -0300

    add meta tags and preview image

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,37 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Frontend Mentor | Pod request access landing page</title>
+        <meta
+          name="description"
+          content="Frontend Mentor: Pod Request Landing Page"
+        />
+        {/* <!-- Site Name, Title, and Description to be displayed --> */}
+        <meta
+          property="og:site_name"
+          content="Frontend Mentor | Pod Request Landing Page "
+        />
+        <meta property="og:title" content="Pod Request Landing Page" />
+        <meta
+          property="og:description"
+          content="My solution to the challenge Pod Request Landing Page, by Frontend Mentor"
+        />
+
+        {/* <!-- Image to display -->  */}
+        {/* <!-- Replace   «example.com/image01.jpg» with your own -->  */}
+        <meta
+          property="og:image"
+          content="https://upbeat-goldwasser-23f0a9.netlify.app/preview.jpg"
+        />
+
+        {/* <!-- No need to change anything here -->  */}
+        <meta property="og:type" content="website" />
+        <meta property="og:image:type" content="image/jpeg" />
+
+        {/* <!-- Website to visit when clicked in fb or WhatsApp-->  */}
+        <meta
+          property="og:url"
+          content="https://upbeat-goldwasser-23f0a9.netlify.app/"
+        ></meta>
       </Head>
       <main>
         <SectionHero />
